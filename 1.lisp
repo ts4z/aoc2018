1972b56de8ac0c233ffd2615c841b9029dd2ba36
Tim Showalter <tjs@psaux.com>
2018-12-03 07:55:30 -0800

    remove 1.lisp; it is junk

--- 1.lisp
+++
@@ -1,8 +0,0 @@
-(defun read-intish (stream) (handler-case (read stream) (t nil)))
-
-(with-open-file (input "input")
-  (let ((n 0))
-    (loop for x = (read-intish input)
-          while x
-          do (incf n x))
-    n))
