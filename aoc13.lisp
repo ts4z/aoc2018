2cd8a67b504bad1c801bb72b5064d5400ad39cac
Tim Showalter <tjs@psaux.com>
2018-12-13 21:40:18 -0800

    non-working day 13 parts 1 and 2
    These *almost* work, but not quite.  They certainly work on the example
    inputs.  This is a rewrite from the previous version -- much less
    functional in style, and easier to reason about, although not quite
    neatly abstracted.

--- aoc13.lisp
+++ aoc13.lisp
@@ -1,3 +1,4 @@
+
 ;; template code; 30% nonsense
 
 (ql:quickload "alexandria")
@@ -11,7 +12,6 @@
 
 
 (defvar +test-input-file+ #P"/home/tjs/git/aoc2018/13-test.input")
-
 
 (defvar +input-file+ #P"/home/tjs/git/aoc2018/13.input")
 
@@ -53,10 +53,11 @@
                                  (#\^ . #\|))))
 
 (defstruct cart
-  dir
-  y
-  x
-  (nt :left))
+  (dir nil :type character)
+  (y nil :type integer)
+  (x nil :type integer)
+  (nt :left :type symbol)
+  (crashed-p nil :type boolean))
 
 (define-condition cant-go-that-way (error)
   ((cart :initarg :cart :reader cart)
@@ -65,12 +66,6 @@
              (format s "can't go that way (cart ~a on track '~a')~&"
                      (cart c) (track c)))))
 
-(define-condition crash (error)
-  ((x :initarg :x :reader x)
-   (y :initarg :u :reader y))
-  (:report (lambda (c s)
-             (format s "crash at ~a,~a~&" (x c) (y c)))))
-
 ;; next nt value
 (defun nnt (nt)
   (switch (nt)
@@ -78,46 +73,90 @@
     (:straight :right)
     (:right    :left)))
 
-(defun move-cart (cart)
-  (let* ((dir (cart-dir cart))
-         (x   (cart-x   cart))
-         (y   (cart-y   cart))
-         (nt  (cart-nt  cart))
+(defvar *location-to-cart*)             ; hash table
+
+(defun cart-loc (c)                     ; key for that hash table
+  (cons (cart-y c) (cart-x c)))
+
+(defmacro cart-at (at)                  ; get cart at given location
+  (check-type at cons)
+  `(gethash ,at *location-to-cart*))
+
+(defun cart-set (c &key nt dir x y)
+  (when (not (cart-crashed-p c))
+
+    (format t "cart ~a~%" c)
+    (format t "nt=~a dir=~a x=~a y=~a~%" nt dir x y)
+
+    (when (or x y)
+      (remhash (cart-loc c) *location-to-cart*))
+
+    (when nt
+      (setf (cart-nt c) nt))
+    (when dir
+      (setf (cart-dir c) dir))
+    (when y
+      (setf (cart-y c) y))
+    (when x
+      (setf (cart-x c) x))
+
+    (when (or x y)
+      (let ((other-cart (cart-at (cart-loc c))))
+        (if other-cart
+            (progn
+              (setf (cart-crashed-p c) t)
+              (setf (cart-crashed-p other-cart) t)
+              (remhash (cart-loc c) *location-to-cart*)
+              (format t "crashed carts: ~a~%" (list c other-cart))
+              (list c other-cart))        ; crashed carts
+            (progn
+              (setf (cart-at (cart-loc c)) c)
+              nil))))))                   ; no cart crashed
+
+(defun move-cart (c)
+  (if (cart-crashed-p c)
+      (assert (not (equal (cart-at (cart-loc c)) c))) ; we should be removed if we're crashed
+      (assert (eq c (cart-at (cart-loc c))))) ; our object should be in the index
+                          
+  (let* ((dir (cart-dir c))
+         (x   (cart-x   c))
+         (y   (cart-y   c))
+         (nt  (cart-nt  c))
          (tk  (aref *grid* y x)))
     (cond
       ((eql tk #\+)
        (switch (dir)
          (#\< (switch (nt)
-                (:left     (make-cart :nt (nnt nt) :dir #\v :x x :y (1+ y)))
-                (:straight (make-cart :nt (nnt nt) :dir dir :x (1- x) :y y))
-                (:right    (make-cart :nt (nnt nt) :dir #\^ :x x :y (1- y)))))
+                (:left     (cart-set c :nt (nnt nt) :dir #\v :y (1+ y)))
+                (:straight (cart-set c :nt (nnt nt) :dir dir :x (1- x)))
+                (:right    (cart-set c :nt (nnt nt) :dir #\^ :y (1- y)))))
          (#\> (switch (nt)
-                (:left     (make-cart :nt (nnt nt) :dir #\^ :x x :y (1- y)))
-                (:straight (make-cart :nt (nnt nt) :dir dir :x (1+ x) :y y))
-                (:right    (make-cart :nt (nnt nt) :dir #\v :x x :y (1+ y)))))
+                (:left     (cart-set c :nt (nnt nt) :dir #\^ :y (1- y)))
+                (:straight (cart-set c :nt (nnt nt) :dir dir :x (1+ x)))
+                (:right    (cart-set c :nt (nnt nt) :dir #\v :y (1+ y)))))
          (#\^ (switch (nt)
-                (:left     (make-cart :nt (nnt nt) :dir #\< :x (1- x) :y y))
-                (:straight (make-cart :nt (nnt nt) :dir dir :x x :y (1- y)))
-                (:right    (make-cart :nt (nnt nt) :dir #\> :x (1+ x) :y y))))
+                (:left     (cart-set c :nt (nnt nt) :dir #\< :x (1- x)))
+                (:straight (cart-set c :nt (nnt nt) :dir dir :y (1- y)))
+                (:right    (cart-set c :nt (nnt nt) :dir #\> :x (1+ x)))))
          (#\v (switch (nt)
-                (:left     (make-cart :nt (nnt nt) :dir #\> :x (1+ x) :y y))
-                (:straight (make-cart :nt (nnt nt) :dir dir :x x :y (1+ y)))
-                (:right    (make-cart :nt (nnt nt) :dir #\< :x (1- x) :y y))))))
+                (:left     (cart-set c :nt (nnt nt) :dir #\> :x (1+ x)))
+                (:straight (cart-set c :nt (nnt nt) :dir dir :y (1+ y)))
+                (:right    (cart-set c :nt (nnt nt) :dir #\< :x (1- x)))))))
        
-      ((and (eql dir #\<) (eql tk #\-)) (make-cart :nt nt :dir dir :x (1- x) :y y))
-      ((and (eql dir #\<) (eql tk #\/)) (make-cart :nt nt :dir #\v :x x :y (1+ y)))
-      ((and (eql dir #\<) (eql tk #\\)) (make-cart :nt nt :dir #\^ :x x :y (1- y)))
-      ((and (eql dir #\>) (eql tk #\-)) (make-cart :nt nt :dir dir :x (1+ x) :y y))
-      ((and (eql dir #\>) (eql tk #\/)) (make-cart :nt nt :dir #\^ :x x :y (1- y)))
-      ((and (eql dir #\>) (eql tk #\\)) (make-cart :nt nt :dir #\v :x x :y (1+ y)))
-      ((and (eql dir #\^) (eql tk #\/)) (make-cart :nt nt :dir #\> :x (1+ x) :y y))
-      ((and (eql dir #\^) (eql tk #\\)) (make-cart :nt nt :dir #\< :x (1- x) :y y))
-      ((and (eql dir #\^) (eql tk #\|)) (make-cart :nt nt :dir dir :x x :y (1- y)))
-      ((and (eql dir #\v) (eql tk #\/)) (make-cart :nt nt :dir #\< :x (1- x) :y y))
-      ((and (eql dir #\v) (eql tk #\\)) (make-cart :nt nt :dir #\> :x (1+ x) :y y))
-      ((and (eql dir #\v) (eql tk #\|)) (make-cart :nt nt :dir dir :x x :y (1+ y)))
+      ((and (eql dir #\<) (eql tk #\-)) (cart-set c          :x (1- x)))
+      ((and (eql dir #\<) (eql tk #\/)) (cart-set c :dir #\v :y (1+ y)))
+      ((and (eql dir #\<) (eql tk #\\)) (cart-set c :dir #\^ :y (1- y)))
+      ((and (eql dir #\>) (eql tk #\-)) (cart-set c          :x (1+ x)))
+      ((and (eql dir #\>) (eql tk #\/)) (cart-set c :dir #\^ :y (1- y)))
+      ((and (eql dir #\>) (eql tk #\\)) (cart-set c :dir #\v :y (1+ y)))
+      ((and (eql dir #\^) (eql tk #\/)) (cart-set c :dir #\> :x (1+ x)))
+      ((and (eql dir #\^) (eql tk #\\)) (cart-set c :dir #\< :x (1- x)))
+      ((and (eql dir #\^) (eql tk #\|)) (cart-set c           :y (1- y)))
+      ((and (eql dir #\v) (eql tk #\/)) (cart-set c :dir #\< :x (1- x)))
+      ((and (eql dir #\v) (eql tk #\\)) (cart-set c :dir #\> :x (1+ x)))
+      ((and (eql dir #\v) (eql tk #\|)) (cart-set c          :y (1+ y)))
 
-      (t (error 'cant-go-that-way :cart cart :track tk)))))
+      (t (error 'cant-go-that-way :cart c :track tk)))))
 
 (defvar *to-next-cell*
   (alexandria:alist-hash-table
@@ -137,7 +176,10 @@
                   (push (make-cart :x x :y y :dir sq) carts)
                   (setf (aref *grid* y x) maybe-track)))))
     (setf *carts* carts)
-    carts))
+    (setf *location-to-cart* (make-hash-table :test 'equal))
+    (mapcar (lambda (c)
+              (setf (cart-at (cart-loc c)) c))
+            *carts*)))
 
 (defun reset-exp ()
   (load-input-grid-for-exp)
@@ -154,55 +196,33 @@
 (defun cart-order-p (c1 c2)
   (or (< (cart-y c1) (cart-y c2))
       (< (cart-x c1) (cart-x c2))))
-      
-(defun tick-once ()
-  (let (crashes
-        moved)
-    (setf *carts* (sort *carts* #'cart-order-p))
-    (loop :with unmoved = *carts*
-          :while (and (null crashes) unmoved)
-          :for moved-cart = (move-cart (car unmoved))
-          :do (setf crashes (crash-check-among moved unmoved))
-          :do (if crashes (return crashes))
-          :do (push moved-cart moved)
-          :do (setf unmoved (cdr unmoved)))
-    (setf *carts* moved)
-    crashes))
-            
-(defun crash-check ()
-  (loop :with crashes = ()
-        :with ht = (make-hash-table :test 'equal)
-        :for cart in *carts*
-        :for x = (cart-x cart)
-        :for y = (cart-y cart)
-        :for p = (cons y x)
-        :for old-occupant = (gethash p ht)
-        :do (if old-occupant
-                (push `(,p ,cart ,old-occupant) crashes)
-                (setf (gethash p ht) cart))
-        :finally (return crashes)))
 
-(defun crash-check-among (&rest whatever)
-  (loop :with crashes = ()
-        :with ht = (make-hash-table :test 'equal)
-        :for cart in (apply #'append whatever)
-        :for x = (cart-x cart)
-        :for y = (cart-y cart)
-        :for p = (cons y x)
-        :for old-occupant = (gethash p ht)
-        :do (if old-occupant
-                (push `(,p ,cart ,old-occupant) crashes)
-                (setf (gethash p ht) cart))
-        :finally (return crashes)))
+(defun same-location-p (c1 c2)
+  (and (eql (cart-x c1) (cart-x c2))
+       (eql (cart-y c1) (cart-y c2))))
+
+(defun running-carts ()
+  (sort (hash-table-values *location-to-cart*)  #'cart-order-p))
+
+(defun tick-once ()
+  (loop :for cart :in (running-carts)
+        ;; :do (format t "move cart: ~a~%" cart)
+        :appending (move-cart cart)))
 
 (defun tick-until-crash ()
-  (loop :for i :from 0
+  (loop :for i :from 0 :below 1000
         :for crashes = (tick-once)
         :while (null crashes)
-        :do (format t "~%~%iteration ok: ~a~%" i)
-        :do (print-grid-with-carts)
+        ;; :do (print-grid-with-carts)
         :finally (return (list i crashes))))
-            
+
+(defun tick-until-one-left ()
+  (loop :for i :from 1
+        :while (> (length (running-carts)) 1)
+        :for crashes = (tick-once)
+        ;; :do (print-grid-with-carts)
+        :finally (return (list i (running-carts)))))
+
 (defun print-this-grid (grid)
   (loop :for y :from 0 :below (array-dimension grid 0) :do
     (loop :for x :from 0 :below (array-dimension grid 1)
@@ -213,14 +233,10 @@
   (print-this-grid (put-carts-on-grid (copy-array *grid*))))
 
 (defun put-carts-on-grid (g)
-  (loop :for c :in *carts*
+  (loop :for c :in (running-carts)
         :do (setf (aref g (cart-y c) (cart-x c)) (cart-dir c)))
-  (loop :for crash in (crash-check)
-        :do (setf (aref g (caar crash) (cdar crash)) #\X))
   g)
 
 (defun tick-once-and-print ()
-  (if (crash-check)
-      "but there is a crash"
-      (prog1 (tick-once)
-             (print-grid-with-carts))))
+  (prog1 (tick-once)
+    (print-grid-with-carts)))
